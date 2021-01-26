2e984076586d5b7104f38eb1cad04abb3fac0bf1
Brian crisp <bcrisp084@yahoo.com>
2021-01-25 19:39:08 -0500

    finished function for uv index and bug fixes

--- assets/script.js
+++ assets/script.js
@@ -6,17 +6,17 @@ var searchInput;
 var APIKey = "7ce604287f42a2c24e790725482afef6";
 
 function displaySaved() {
-  let savedCities = JSON.parse(localStorage.getItem("cities"));
-  if (savedCities) {
-    cities = savedCities;
+  let citiesSaved = JSON.parse(localStorage.getItem("cities"));
+  if (citiesSaved) {
+    cities = citiesSaved;
   } else {
     cities = [];
   }
-  $("#forecastRow").empty();
+  cities.forEach((city) => createButtons(city));
 }
 
-function savedCities(cities) {
-  localStorage.setItem("cities", JSON.stringify(cities));
+function savedCities(citiesToSave) {
+  localStorage.setItem("cities", JSON.stringify(citiesToSave));
 }
 
 clearBtn.addEventListener("click", function () {
@@ -24,48 +24,50 @@ clearBtn.addEventListener("click", function () {
   citiesDiv.innerHTML = "";
 });
 
-function searchCity(searchInput) {
-  displaySaved();
-  cities.push(searchInput);
-  if (searchInput != "") {
-    var queryURL =
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-      searchInput +
-      "&units=imperial&appid=" +
-      APIKey;
-    $.ajax({
-      url: queryURL,
-      method: "GET",
-    }).then(function (response) {
-      var latData = $(response.coord.lat);
-      var lonData = $(response.coord.lon);
-      console.log(response);
-      $("#current-city").text(response.name);
-      $("#current-windspeed").text("Wind Speed: " + response.wind.speed);
-      $("#current-humidity").text("Humidity: " + response.main.humidity);
-      $("#current-temp").text(
-        "Temperature (F) " + response.main.temp.toFixed(2)
-      );
-      $("#uv-index").text("uv-index: " + latData + lonData);
-      $("#weather-image").attr({
-        src:
-          "https://openweathermap.org/img/w/" +
-          response.weather[0].icon +
-          ".png",
-        height: "100px",
-        width: "100px",
-      });
-    });
-    savedCities(cities);
-  } else {
+function searchCity(searchInput, addToHistory = true) {
+  if (!searchInput) {
     alert("please enter a city");
+    return false;
   }
+
+  $("#forecastRow").empty();
+  if (addToHistory) {
+    createButtons(searchInput);
+    cities.push(searchInput);
+  }
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    searchInput +
+    "&units=imperial&appid=" +
+    APIKey;
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response.coord.lon);
+    console.log(response.coord.lat);
+    var latData = response.coord.lat;
+    var lonData = response.coord.lon;
+    $("#current-city").text(response.name);
+    $("#current-windspeed").text("Wind Speed: " + response.wind.speed);
+    $("#current-humidity").text("Humidity: " + response.main.humidity);
+    $("#current-temp").text("Temperature (F) " + response.main.temp.toFixed(2));
+    $("#weather-image").attr({
+      src:
+        "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png",
+      height: "100px",
+      width: "100px",
+    });
+    getuvIndex(latData, lonData);
+  });
+
+  savedCities(cities);
+
   var queryURL5 =
     "https://api.openweathermap.org/data/2.5/forecast?q=" +
     searchInput +
     "&units=imperial&appid=" +
     APIKey;
-
   $.ajax({
     url: queryURL5,
     method: "GET",
@@ -73,7 +75,6 @@ function searchCity(searchInput) {
     for (i = 0; i < response5.list.length; i++) {
       if (response5.list[i].dt_txt.split(" ")[1] === "18:00:00") {
         var forecastEl = $("<div class='col' id='fiveDay'>");
-
         var dateEl = $("<h6>").append(
           "Date: " + response5.list[i].dt_txt.split(" ")[0]
         );
@@ -86,7 +87,6 @@ function searchCity(searchInput) {
         var humidityEl = $("<h6>").append(
           "Humidity: " + response5.list[i].main.humidity
         );
-
         forecastEl.append(dateEl, iconEl, tempEl, humidityEl);
       }
       $("#forecastRow").append(forecastEl);
@@ -98,39 +98,8 @@ $("#search").on("click", function (event) {
   event.preventDefault();
   var searchInput = $("#searchInput").val();
   searchCity(searchInput);
-  createButtons(searchInput);
   $("#forecastRow").empty();
 });
-
-function getUV(latData, lonData) {
-  var queryURL =
-    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-    latData +
-    "&lon=" +
-    lonData +
-    "&exclude=minutely,hourly,daily,alerts&appid=" +
-    APIKey;
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-  }).then(function (uvresponse) {
-    searchCity(input);
-    var uvresponse = lonData;
-    var uvresponse = latData;
-    if (uvresponse < 3) {
-      $(this).addClass("low");
-    } else if (uv < 6) {
-      $(this).addClass("moderate");
-    } else if (uv < 8) {
-      $(this).addClass("high");
-    } else if (uv < 11) {
-      $(this).addClass("veryHigh");
-    } else {
-      $(this).addClass("extreme");
-    }
-    $("#uv-index").text(uv);
-  });
-}
 
 function createButtons(searchInput) {
   var btn = $("<button>").text(searchInput);
@@ -138,7 +107,41 @@ function createButtons(searchInput) {
   btn.attr("data-name", searchInput);
   btn.on("click", function (event) {
     event.preventDefault();
-    searchCity(searchInput);
+    searchCity(searchInput, false);
   });
   $("#cities").append(btn);
 }
+function getuvIndex(latData, lonData) {
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/uvi?lat=" +
+    latData +
+    "&lon=" +
+    lonData +
+    "&appid=" +
+    APIKey;
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (uvresponse) {
+    var uv = uvresponse.value;
+    console.log(uvresponse);
+    $("#uv-index").text("uv-index: " + uv);
+    $("#uv-index").removeClass(
+      ["low", "moderate", "high", "veryHigh", "extreme"].join(" ")
+    );
+    if (uv < 3) {
+      $("#uv-index").addClass("low");
+    } else if (uv < 6) {
+      $("#uv-index").addClass("moderate");
+    } else if (uv < 8) {
+      $("#uv-index").addClass("high");
+    } else if (uv < 11) {
+      $("#uv-index").addClass("veryHigh");
+    } else {
+      $("#uv-index").addClass("extreme");
+    }
+  });
+}
+$(document).ready(function () {
+  displaySaved();
+});
